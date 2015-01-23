51a79e777f12de674c938329f8c7b8ff2e14c057
Nawaaz Gujja Shaik <nawaaz.gujjashaik@epfl.ch>
2015-01-23 14:12:11 +0100

    added comments
    comments :D

--- source/plusminus.c
+++ source/plusminus.c
@@ -26,17 +26,18 @@ void plusminus_init() {
 	BGCTRL_SUB[0] = BG_TILE_BASE(1) | BG_MAP_BASE(0) | BG_32x32 | BG_COLOR_16;
 	BGCTRL_SUB[1] = BG_TILE_BASE(3) | BG_MAP_BASE(16) | BG_32x32 | BG_COLOR_16;
 
-	// Copy tiles to memory
+	// Copy tiles to memory. Number tiles in BG0 and game BG in BG1
 	swiCopy(numbersTiles, BG_TILE_RAM_SUB(1), numbersTilesLen/2);
 	swiCopy(plusminus_imTiles, BG_TILE_RAM_SUB(3), plusminus_imTilesLen/2);
 
-	// Copy palette
+	// Copy palette for both images
 	swiCopy(numbersPal, BG_PALETTE_SUB, numbersPalLen/2);
 	swiCopy(plusminus_imPal, &BG_PALETTE_SUB[16], plusminus_imPalLen/2);
 
-	// Same background color
+	// Same background color in both images
 	BG_PALETTE_SUB[1] = BG_PALETTE_SUB[27];
-
+	
+	// Init. Timer for interrupt
 	TIMER0_DATA = TIMER_FREQ_1024(4);
 	TIMER0_CR = TIMER_DIV_1024 | TIMER_IRQ_REQ | TIMER_ENABLE;
 	irqSet(IRQ_TIMER0, &plusminus_timer_ISR);
@@ -73,7 +74,8 @@ void plusminus_new_number() {
 	for(i = 0; i < 3; i++) { new_num[i] = rand()%10; }
 
 	j = rand()%4;
-
+	// Determine if new number will be a multiple, or factor of 2
+	// If yes, compute the necessary value ( * 2 or /2 )
 	if(j == 3) {
 
 		k = rand()%2;
@@ -114,6 +116,7 @@ void plusminus_new_number() {
 
 		}
 	}
+	// Or else load random number and check that it's not the same number
 	else {
 
 		if     (new_num[0] > numbers[0]) 	{ compare = GREATER; }
@@ -125,7 +128,8 @@ void plusminus_new_number() {
 
 
 	}
-
+	
+	//Load generated new number into number array
 	for(i = 0; i < 3; i++) 	{ numbers[i] = new_num[i]; }
 
 
@@ -134,6 +138,8 @@ void plusminus_new_number() {
 void plusminus_draw(void) {
 
 	int row, col;
+	
+	// Draw the proper number at the proper position
 
 	//First number
 	for(row = 7; row < 17; row++) {
@@ -163,6 +169,7 @@ bool plusminus_game(void) {
 	u16 keys = (u16) keysDown();
 
 	// Stop game if START button pressed
+	// Else check where pressed, and if correct
 	if(keys & KEY_START) { return true; }
 	else if(keys & KEY_TOUCH) {
 
@@ -206,8 +213,11 @@ bool plusminus_game(void) {
 void plusminus_correct(void) {
 
 	pm_score++;
-
+	// If correct, increment score,
+	// If correctly found multiple or factor, more points!
 	if(compare > 2) { pm_score = pm_score + 2; }
+	
+	// Generate CORRECT condition
 
 	int old_color = BG_PALETTE_SUB[1];
 
@@ -229,7 +239,9 @@ void plusminus_correct(void) {
 void plusminus_wrong(void) {
 
 	if(pm_score > 0)	{ pm_score--; }
-
+	// Remove points
+	
+	// Generate WRONG condition
 	int old_color = BG_PALETTE_SUB[1];
 
 	BG_PALETTE_SUB[1] = TRUERED;
